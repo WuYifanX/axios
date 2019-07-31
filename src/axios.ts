dd554c9945274dd1eae93f32628dce14bc4a49ef
WuYifanX <510182912@qq.com>
2019-07-31 23:45:40 +0800

    feat(helpers and examples): add transformURL component

--- src/axios.ts
+++ src/axios.ts
@@ -1,8 +1,19 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import buildURL from './helpers/url'
 
 const axios = (config: AxiosRequestConfig): void => {
+  processConfig(config)
   xhr(config)
+}
+
+const processConfig = (config: AxiosRequestConfig) => {
+  config.url = transformURL(config)
+}
+
+const transformURL = (config: AxiosRequestConfig): string => {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
